b45e3f5a1718e19fae01f15e9a5eba7683e2f26f
Corey Bright <95330156+cbrig5@users.noreply.github.com>
2025-06-19 16:51:29 -0400

    [FIX] backend: fixed syntax error

--- backend/backend/deployment.py
+++ backend/backend/deployment.py
@@ -2,7 +2,7 @@ import os
 from .settings import *
 from .settings import BASE_DIR
 
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
+ALLOWED_HOSTS = [os.environ.get('WEBSITE_HOSTNAME')]
 CSRF_TRUSTED_ORIGINS = ['https//' + os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
@@ -32,7 +32,7 @@ STORAGES = {
     }
 }
 
-CONNECTION = os.environ('AZURE_POSTGRESQL_CONNECTIONSTRING')
+CONNECTION = os.environ.get('AZURE_POSTGRESQL_CONNECTIONSTRING')
 CONNECTION_STR = {pair.split('=')[0]: pair.split('=')[1] for pair in CONNECTION.split(' ')}
 
 DATABASES = {
