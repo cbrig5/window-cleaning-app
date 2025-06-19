27e09072bd30ee28768e3d7ff140998bf06a3c39
Corey Bright <95330156+cbrig5@users.noreply.github.com>
2025-06-19 17:58:46 -0400

    Revert "[ADD] backend: added Quote and Client class and build_script.sh"

--- backend/website/admin.py
+++ backend/website/admin.py
@@ -1,23 +1,6 @@
 from django.contrib import admin
-from .models import Client, Service, Quote, WebsiteForm
+from .models import WebsiteForm, Service
 
 admin.site.register(WebsiteForm)
 admin.site.register(Service)
-
-@admin.register(Client)
-class ClientAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'phone')
-    search_fields = ('name', 'email')
-    ordering = ('name', 'created_on')
-
-@admin.register(Quote)
-class QuoteAdmin(admin.ModelAdmin):
-    list_display = ('client_name', 'estimated_price', 'sent_on')
-    search_fields = ('client__name', 'service__code')
-    list_filter = ('sent_on',)
-    ordering = ('-sent_on',)
-
-    def client_name(self, obj):
-        return obj.client.name
-    client_name.short_description = 'Client Name'
-    client_name.admin_order_field = 'client__name'
+# Register your models here.
--- backend/website/models/__init__.py
+++ backend/website/models/__init__.py
@@ -1,4 +1,2 @@
 from .service import Service
 from .website_form import WebsiteForm
-from .quote import Quote
-from .client import Client
