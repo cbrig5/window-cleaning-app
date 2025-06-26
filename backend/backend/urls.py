1674a7758552e570ca5dafe8d68330fe6bd4b050
Corey Bright <95330156+cbrig5@users.noreply.github.com>
2025-06-26 18:20:52 -0400

    [FIX] fixed bad imports and routes

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,11 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from website.views.project_view import ProjectViewSet, ProjectManagerViewSet
 
 router = DefaultRouter()
-router.register(r'project', ProjectViewSet)
-router.register(r'projectmanager', ProjectManagerViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
