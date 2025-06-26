a87974f2639d088a9a38b56a559bbc00a3ce4a21
Corey Bright <95330156+cbrig5@users.noreply.github.com>
2025-06-26 19:04:07 -0400

    [DEBUG] commit to debug startup error

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -8,10 +8,17 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+import traceback
 
 from django.core.wsgi import get_wsgi_application
 
 settings_module = 'backend.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'backend.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
-application = get_wsgi_application()
+try:
+    application = get_wsgi_application()
+except Exception:
+    sys.stderr.write("WSGI startup failed:\n")
+    traceback.print_exc()
+    raise
